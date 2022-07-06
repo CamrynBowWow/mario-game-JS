0597c40555a0c55a67e525887e47ca1438c96309
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-06 22:35:17 +0200

    Player creation done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100,
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player();
+player.draw();
