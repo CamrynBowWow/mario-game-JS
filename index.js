98c6cba153a40146d5b37e1d1e229f1504428148
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-07 09:37:34 +0200

    Player movement done

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ class Player {
         context.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
+    // Gravity code to pull player back down
     update() {
         this.draw();
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height) {
@@ -40,12 +42,74 @@ class Player {
 }
 
 const player = new Player();
-player.draw();
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+
+    if (keys.right.pressed) {
+        player.velocity.x = 2;
+    } else if (keys.left.pressed) {
+        player.velocity.x = -2;
+    } else {
+        player.velocity.x = 0;
+    }
 }
 
 animate();
+// TODO Work on player movement as it is not good
+// Make W key a keypress and remove some keys to make it more static 
+window.addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65: // A key
+            console.log('left');
+            keys.left.pressed = true;
+            break;
+
+        case 83: // S key
+            console.log('down')
+            break;
+
+        case 68: // D key
+            console.log('right')
+            keys.right.pressed = true;
+            break;
+
+        case 87: // W key
+            console.log('up')
+            player.velocity.y -= 20;
+            break;
+    }
+})
+
+window.addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65: // A key
+            console.log('left');
+            keys.left.pressed = false;
+            break;
+
+        case 83: // S key
+            console.log('down')
+            break;
+
+        case 68: // D key
+            console.log('right')
+            keys.right.pressed = false;
+            break;
+
+        case 87: // W key
+            console.log('up')
+            break;
+    }
+})
