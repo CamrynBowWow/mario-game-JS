3d56b67cb9b747eb5f6a91ca27c765aa9bfb5504
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-09 08:24:11 +0200

    GenericObject file created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { createImage, platform, hills, background, platformSmallTall } from './CreateImage.js';
 import { Player } from './Player.js';
 import { Platform } from './Platform.js';
+import { GenericObject } from './GenericObject.js';
 
 export const canvas = document.querySelector('canvas');
 export const context = canvas.getContext('2d');
@@ -9,24 +10,6 @@ canvas.width = 1024;
 canvas.height = 576;
 
 export const gravity = 0.8;
-
-class GenericObject {
-	constructor({ xPos, yPos, image }) {
-		this.position = {
-			x: xPos,
-			y: yPos,
-		};
-
-		this.image = image;
-
-		this.width = image.width;
-		this.height = image.height;
-	}
-
-	draw() {
-		context.drawImage(this.image, this.position.x, this.position.y);
-	}
-}
 
 let platformImage = createImage(platform);
 let platformSmallTallImage = createImage(platformSmallTall);
