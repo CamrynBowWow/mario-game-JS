2af1d34210129f7ed56b1bb5a2a7bce056c70b51
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-17 00:36:41 +0200

    Audio fix for live

--- MusicPlayer.js
+++ MusicPlayer.js
@@ -1,7 +1,10 @@
 const myMusic = document.querySelector('#gameSound');
 
+let audio = new Audio(myMusic.currentSrc);
+
 export function playMusic() {
-	myMusic.volume = 0.015;
-	myMusic.play();
-	myMusic.loop = true;
+	audio.volume = 0.02;
+	audio.muted = false;
+	audio.play();
+	audio.loop = true;
 }
