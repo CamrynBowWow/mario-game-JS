1554afb6ec13203e7b56fc7ade1bd6fe8fdcd8c6
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-20 23:50:36 +0200

    Music volume increased

--- MusicPlayer.js
+++ MusicPlayer.js
@@ -3,7 +3,7 @@ const myMusic = document.querySelector('#gameSound');
 let audio = new Audio(myMusic.currentSrc);
 
 export function playMusic() {
-	audio.volume = 0.02;
+	audio.volume = 0.3;
 	audio.muted = false;
 	audio.play();
 	audio.loop = true;
